08fe77d0b4c37578cd829c3a8600e86e6b96a1c9
Artem Kuznetsov <artemkuz1083@gmail.com>
2023-10-25 11:05:54 +0500

    lab 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+class Program
+{
+    public static void Main()
+    {
+        GetAnswer(GetLists(Console.ReadLine()));
+    }
+
+    public static ArrayList GetLists(string expression)
+    {
+        expression = expression.Replace(" ", string.Empty);
+
+        List<string> numbers = new List<string>();
+        List<char> signs = new List<char>();
+        string toBuildNumber = "";
+
+        foreach (char symbol in expression)
+        {
+            if (Char.IsDigit(symbol))
+            {
+                toBuildNumber += symbol;
+            }
+            else
+            {
+                numbers.Add(toBuildNumber);
+                toBuildNumber = "";
+                signs.Add(symbol);
+            }
+        }
+        numbers.Add(toBuildNumber);
+
+        Console.Write("Your numbers: ");
+        foreach (string number in numbers) { Console.Write($"{number} "); }
+        Console.WriteLine();
+        Console.Write("Your signs: ");
+        foreach (char sign in signs) { Console.Write($"{sign} "); }
+        Console.WriteLine();
+
+        ArrayList lists = new ArrayList
+        {
+            numbers,
+            signs
+        };
+        return lists;
+    }
+
+    public static void GetAnswer(ArrayList lists)
+    {
+        List<string> numbers = (List<string>)lists[0];
+        List<char> signs = (List<char>)lists[1];
+
+        while (numbers.Count != 1)
+        {
+            for (int i = 0; i < signs.Count; i++)
+            {
+                double number = 0;
+
+                if (signs[i] == '/' | signs[i] == '*')
+                {
+
+                    if (signs[i] == '/')
+                    {
+                        number = double.Parse(numbers[i]) / double.Parse(numbers[i + 1]);
+                    }
+
+                    else
+                    {
+                        number = double.Parse(numbers[i]) * double.Parse(numbers[i + 1]);
+                    }
+
+                    numbers.RemoveAt(i + 1);
+                    numbers.RemoveAt(i);
+                    numbers.Insert(i, $"{number}");
+                    signs.RemoveAt(i);
+                }
+
+                else if (!(signs.Contains('/') || signs.Contains('*')))
+                {
+
+                    if (signs[i] == '+')
+                    {
+                        number = double.Parse(numbers[i]) + double.Parse(numbers[i + 1]);
+                    }
+
+                    else if (signs[i] == '-')
+                    {
+                        number = double.Parse(numbers[i]) - double.Parse(numbers[i + 1]);
+                    }
+
+                    numbers.RemoveAt(i + 1);
+                    numbers.RemoveAt(i);
+                    numbers.Insert(i, $"{number}");
+                    signs.RemoveAt(i);
+                }
+            }
+
+        }
+
+        Console.Write("Answer is ");
+        foreach (string number in numbers)
+        {
+            Console.Write($"{number} ");
+        }
+    }
+}
---
+++ Lesson1/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Xml.Schema;
+
+namespace Lesson1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите выражение: ");
+            string str = Console.ReadLine();
+            str = str.Replace(" ",string.Empty);
+            OperationAndNumbers(str);
+        }
+
+        public static void OperationAndNumbers(string str)
+        {
+            string[] arrayNum = str.Split(new char[] {'+', '-', '*', '/', '(', ')'},StringSplitOptions.RemoveEmptyEntries);
+            List<double> numbers = new List<double>();
+            List<Char> operation = new List<Char>();
+            char[] testOperations = {'+', '-', '*', '/', '(', ')'};
+            foreach(string num in arrayNum)
+            {
+                numbers.Add(Convert.ToDouble(num));
+            }
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (testOperations.Contains(str[i]))
+                {
+                    operation.Add(str[i]);
+                }
+            }
+            Console.WriteLine($"список чисел: {string.Join(" ", numbers)}");
+            Console.WriteLine($"список операций: {string.Join(" ", operation)}");
+            Operation(numbers, operation);
+        }
+
+        public static void Operation(List<double> num, List<Char> oper)
+        {
+            var priorety = Priorety(oper);
+            while (oper.Contains('(') && oper.Contains(')'))
+            {
+                oper.Remove('(');
+                oper.Remove(')');
+            }
+
+            while (num.Count != 1)
+            {
+                int ind = priorety.IndexOf(priorety.Max());
+                priorety.RemoveAt(ind);
+
+                var op = oper[ind];
+                var first = num[ind];
+                var second = num[ind + 1];
+                num.RemoveAt(ind);
+                num.RemoveAt(ind);
+                oper.RemoveAt(ind);
+
+                var result = Calculate(op, first, second);
+                num.Insert(ind, result);
+            }
+            Console.WriteLine($"Значение выражения: {string.Join(" ", num)}");
+        }
+
+        public static List<int> Priorety(List<char> oper)
+        {
+            List<int> prioretyList = new List<int>();
+            int prioret = 1;
+            foreach (var ch in oper)
+            {
+                if(ch == '(')
+                {
+                    prioret += 2;
+                    continue;
+                }
+                else if (ch == ')')
+                {
+                    prioret -= 2;
+                    continue;
+                }
+                if(ch == '*' ||  ch == '/')
+                {
+                    prioretyList.Add(prioret + 1);
+                    continue;
+                }
+                prioretyList.Add(prioret);
+            }
+            return prioretyList;
+        }
+
+        public static double Calculate(int op, double first, double second)
+        {
+            switch (op)
+            {
+                case '*': return first * second;
+                case '/': return first / second;
+                case '+': return first + second;
+                case '-': return first - second;
+                default: return double.NaN;
+            }
+        }
+
+    }
+}
