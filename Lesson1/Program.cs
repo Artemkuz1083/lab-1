40e5257946231cc65fb10b8bfdbea8876ddfc0c7
Artem Kuznetsov <artemkuz1083@gmail.com>
2023-11-03 00:30:15 +0500

    +RPN but calculations don't work

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections.Generic;
 using System.Xml.Schema;
 
 namespace Lesson1
@@ -15,82 +14,91 @@ namespace Lesson1
             Console.Write("Введите выражение: ");
             string str = Console.ReadLine();
             str = str.Replace(" ",string.Empty);
-            OperationAndNumbers(str);
+            var expression = OperationAndNumbers(str);
+            var prn = PRN(expression);
+            Console.WriteLine(string.Join(" ", prn));
+
         }
 
-        public static void OperationAndNumbers(string str)
+        public static List<object> OperationAndNumbers(string str)
         {
             char[] testOperations = { '+', '-', '*', '/', '(', ')' };
-            string[] arrayNum = str.Split(testOperations,StringSplitOptions.RemoveEmptyEntries);
-            List<double> numbers = new List<double>();
-            List<Char> operation = new List<Char>();
-            foreach(string num in arrayNum)
-            {
-                numbers.Add(Convert.ToDouble(num));
-            }
+            List<object> expression = new List<object>();
+            string num = string.Empty;
             for (int i = 0; i < str.Length; i++)
             {
                 if (testOperations.Contains(str[i]))
                 {
-                    operation.Add(str[i]);
+                    if (num != string.Empty)
+                    {
+                        expression.Add(num);
+                        num = string.Empty;
+                    }
+                    expression.Add(str[i]);
+                }
+                else
+                {
+                    num += str[i];
                 }
             }
-            Console.WriteLine($"список чисел: {string.Join(" ", numbers)}");
-            Console.WriteLine($"список операций: {string.Join(" ", operation)}");
-            Operation(numbers, operation);
+            expression.Add(num);
+            return expression;
         }
 
-        public static void Operation(List<double> num, List<Char> oper)
+        public static List<object> PRN(List<object> expression) 
         {
-            var priorety = Priorety(oper);
-            while (oper.Contains('(') && oper.Contains(')'))
+            Dictionary<object, int> prioretyDictionary = new Dictionary<object, int>
             {
-                oper.Remove('(');
-                oper.Remove(')');
-            }
-
-            while (num.Count != 1)
+                {'+', 1},
+                {'-', 1},
+                {'*', 2},
+                {'/', 2},
+                {'(', 0},
+                {')', 5},
+            };
+            List<object> prn = new List<object>();
+            Stack<object> stack = new Stack<object>();
+            foreach (var item in expression)
             {
-                int ind = priorety.IndexOf(priorety.Max());
-                priorety.RemoveAt(ind);
-
-                var op = oper[ind];
-                var first = num[ind];
-                var second = num[ind + 1];
-                num.RemoveAt(ind);
-                num.RemoveAt(ind);
-                oper.RemoveAt(ind);
-
-                var result = Calculate(op, first, second);
-                num.Insert(ind, result);
+                if (prioretyDictionary.ContainsKey(item))
+                {
+                    if ((Char)item == ')')
+                    {
+                        while ((Char)stack.Peek() != '(')
+                        {
+                            prn.Add(stack.Pop());
+                        }
+                        stack.Pop();
+                    }
+                    else if (stack.Count == 0
+                        || (Char)item == '('
+                        || prioretyDictionary[item] > prioretyDictionary[stack.Peek()])
+                    {
+                        stack.Push(item);
+                    }
+                    else if (prioretyDictionary[item] <= prioretyDictionary[stack.Peek()])
+                    {
+                        while (stack.Count > 0)
+                        {
+                            prn.Add(stack.Pop());
+                        }
+                        stack.Push(item);
+                    }
+                }
+                else
+                {
+                    prn.Add(item);
+                }
             }
-            Console.WriteLine($"Значение выражения: {string.Join(" ", num)}");
-        }
-
-        public static List<int> Priorety(List<char> oper)
-        {
-            List<int> prioretyList = new List<int>();
-            int prioret = 1;
-            foreach (var ch in oper)
+            while (stack.Count > 0)
             {
-                if(ch == '(')
-                {
-                    prioret += 2;
-                    continue;
-                }
-                else if (ch == ')')
-                {
-                    prioret -= 2;
-                    continue;
-                }
-                if(ch == '*' ||  ch == '/')
-                {
-                    prioretyList.Add(prioret + 1);
-                    continue;
-                }
-                prioretyList.Add(prioret);
+                prn.Add(stack.Pop());
             }
-            return prioretyList;
+            while(prn.Contains(string.Empty))
+            {
+                prn.Remove(string.Empty);
+            }
+            return prn;
         }
 
         public static double Calculate(int op, double first, double second)
@@ -104,6 +112,5 @@ namespace Lesson1
                 default: return double.NaN;
             }
         }
-
     }
 }
