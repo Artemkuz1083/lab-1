e300d260a84a356c24dacc3582db2829742dadd9
Artem Kuznetsov <artemkuz1083@gmail.com>
2023-11-23 12:15:36 +0500

    OOP

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,27 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Security.Authentication.ExtendedProtection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Schema;
 
 namespace Lesson1
 {
-    internal class Program
+ 
+    class Token
     {
-        static void Main(string[] args)
+
+    }
+
+    class Number : Token
+    {
+        public double Symbol;
+
+        public Number(double num)
         {
-            Console.Write("Введите выражение: ");
-            string str = Console.ReadLine();
-            str = str.Replace(" ",string.Empty);
-            var prn = PRN(str);
-            Console.Write("ОПЗ: ");
-            Console.WriteLine(string.Join(" ", prn));
-            Console.Write("Значение: ");
-            Console.WriteLine(string.Join(" ", Result(prn)));
+            Symbol = num;
+        }
+    }
+
+    class Operation : Token
+    {
+        public char Symbol;
+        public int Priorety;
+        public Operation(char symbol)
+        {
+            Symbol = symbol;
+            Priorety = GetPriorety(symbol);
         }
 
-        public static List<object> PRN(string str) 
+        private int GetPriorety(char symbol)
         {
             Dictionary<object, int> prioretyDictionary = new Dictionary<object, int>
             {
@@ -32,48 +46,129 @@ namespace Lesson1
                 {'(', 0},
                 {')', 5},
             };
-            List<object> prn = new List<object>();
-            Stack<object> stack = new Stack<object>();
+            return prioretyDictionary[symbol];
+        }
+    }
+
+    class Brackets : Token
+    {
+        public char Symbol;
+        public bool IsClosing;
+        public Brackets(char symbol)
+        {
+            if (symbol != '(' && symbol != ')')
+                throw new ArgumentException("This is not valid bracket");
+
+            IsClosing = symbol == ')';
+            Symbol = symbol;
+        }
+    }
+
+    class RPN
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите выражение: ");
+            string str = Console.ReadLine();
+            str = str.Replace(" ", string.Empty);
+            var tokens = GetToken(str);
+            var prn = PRN(tokens);
+            Console.Write("Значение: ");
+            Console.WriteLine(string.Join(" ", Result(prn)));
+        }
+
+        public static List<Token> GetToken(string str)
+        {
+            List<Token> tokens = new List<Token>();
             string num = string.Empty;
-            for (int i = 0; i < str.Length; i++)
+            for(int i = 0; i < str.Length; i++)
             {
-                if (prioretyDictionary.ContainsKey(str[i]))
+                if (char.IsDigit(str[i]))
+                {
+                    num += str[i];
+                }
+                else if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/')
+                {
+                    if(num != string.Empty)
+                    {
+                        tokens.Add(new Number(double.Parse(num)));
+                        num = string.Empty;
+                    }
+                    tokens.Add(new Operation(str[i]));
+                }
+                else if (str[i] == '(' || str[i] == ')')
                 {
                     if (num != string.Empty)
                     {
-                        prn.Add(num);
+                        tokens.Add(new Number(double.Parse(num)));
                         num = string.Empty;
                     }
+                    tokens.Add(new Brackets(str[i]));
+                }
+            }
+            if (num != string.Empty)
+            {
+                tokens.Add(new Number(double.Parse(num)));
+            }
+            return tokens;
+        }
 
-                    if (str[i] == ')')
+
+
+        public static List<Token> PRN(List<Token> tokens)
+        {
+            List<Token> prn = new List<Token>();
+            Stack<Token> stack = new Stack<Token>();
+            foreach(Token token in tokens)
+            {
+                if(stack.Count == 0 && !(token is Number))
+                {
+                    stack.Push(token);
+                    continue;
+                }
+                if (token is Operation)
+                {
+                    if (stack.Peek() is Brackets)
                     {
-                        while ((Char)stack.Peek() != '(')
+                        stack.Push(token);
+                        continue;
+                    }
+
+                    Operation oper = (Operation)token;
+                    Operation oper2 = (Operation)stack.Peek();
+                    if (oper.Priorety > oper2.Priorety)
+                    {
+                        stack.Push(token);
+                    }
+                    else if (oper.Priorety <= oper2.Priorety)
+                    {
+                        while (stack.Count > 0 && !(token is Brackets))
+                        {
+                            prn.Add(stack.Pop());
+                        }
+                        stack.Push(token);
+                    }
+                }
+                else if (token is Brackets)
+                {
+                    if (((Brackets)token).IsClosing)
+                    {
+                        while (!(stack.Peek() is Brackets))
                         {
                             prn.Add(stack.Pop());
                         }
                         stack.Pop();
                     }
-                    else if (stack.Count == 0
-                        || str[i] == '('
-                        || prioretyDictionary[str[i]] > prioretyDictionary[stack.Peek()])
+                    else 
                     {
-                        stack.Push(str[i]);
-                    }
-                    else if (prioretyDictionary[str[i]] <= prioretyDictionary[stack.Peek()])
-                    {
-                        while (stack.Count > 0 && (Char)stack.Peek() != '(')
-                        {
-                            prn.Add(stack.Pop());
-                        }
-                        stack.Push(str[i]);
+                        stack.Push(token);
                     }
                 }
-                else
+                else if(token is Number)
                 {
-                    num += str[i];
+                    prn.Add(token);
                 }
             }
-            prn.Add(num);
             while (stack.Count > 0)
             {
                 prn.Add(stack.Pop());
@@ -81,29 +176,30 @@ namespace Lesson1
             return prn;
         }
 
-        public static Stack<double> Result(List<object> expression)
+        public static Stack<double> Result(List<Token> expression)
         {
             Stack<double> stack = new Stack<double>();
-            for (var i =0; i < expression.Count; i++)
+            foreach (Token token in expression)
             {
-                if (expression[i] is string)
+                if (token is Number number)
                 {
-                    double num = Convert.ToDouble(expression[i]);
-                    stack.Push(num);
+                    number = (Number)token;
+                    stack.Push(number.Symbol);
                 }
                 else
                 {
                     var second = stack.Pop();
                     var first = stack.Pop();
-                    stack.Push(Calculate((Char)expression[i], first, second));
+                    stack.Push(Calculate(token, first, second));
                 }
             }
             return stack;
         }
 
-        public static double Calculate(Char op, double first, double second)
+        public static double Calculate(Token op, double first, double second)
         {
-            switch (op)
+            var oper = (Operation)op;
+            switch (oper.Symbol)
             {
                 case '*': return first * second;
                 case '/': return first / second;
@@ -113,4 +209,5 @@ namespace Lesson1
             }
         }
     }
+    
 }
